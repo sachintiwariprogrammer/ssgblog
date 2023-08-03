0e2b723904d6a9a10dea9a957b79580e497a4ae8
No Name <sachintiwariprogrammer@gmail.com>
2023-08-03 14:01:24 +0000

    Updated 1 file via CloudCannon.

--- cloudcannon.config.js
+++ cloudcannon.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-    collections_config:{
-        posts:{
-            name: "Posts"
-        }
-    }
-}
+  collections_config: {
+    posts: {
+      path: '_posts',
+      parser: 'front-matter',
+      url: (filePath, parsed, { filters }) => {
+        const year = new Date(parsed.date).getFullYear();
+        const slug = filters.slugify(parsed.title || '');
+        return `/posts/${id}/`;
+      },
+      output: true
+    },
+  },
+};
