2dac9c49028d06aae1e19e4cf138fc898ade2893
sachintiwariprogrammer <141241572+sachintiwariprogrammer@users.noreply.github.com>
2023-08-03 18:19:43 +0530

    changes

--- src/pages/posts/[postid].js
+++ src/pages/posts/[postid].js
@@ -1,4 +1,3 @@
-
 import Head from 'next/head'
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
@@ -7,52 +6,51 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import Link from 'next/link'
+import Link from 'next/link';
+
+export default function BlogCard({blogData}) {
 
 
-
-export const getServerSideProps = async ({ params: { postid } }) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postid}`)
-    const post = await res.json()
-    return { 
-        props: { 
-            post 
-        } 
-    }
-}
-
-export default function BlogCard({post}) {
-
-    console.log(post);
   return (
     <>
-    <Head>
-        <title>My Blog</title>
-        <meta name="description" content="Developed by Sachin Tiwari" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    <main>
-        <Box>
-            <Grid container justifyContent="center" rowSpacing={3}>
-                <Grid item xs={8}>
-                <Card>
-                    <CardContent>
-                        {post.title && 
-                            <Typography gutterBottom variant="h5" component="div">{post.title}</Typography>
-                        }
-                        {post.body && 
-                            <Typography gutterBottom variant="p" component="div">{post.body}</Typography>
-                        }
-                    </CardContent>
-                    <CardActions>
-                        <Link href="/"><Button size="small">Homepage</Button></Link>
-                    </CardActions>
-                </Card>
+      <Head>
+            <title>{blogData.title?blogData.title:""}</title>
+            <meta name="description" content="Developed by Sachin Tiwari" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+            <Box>
+                <Grid container justifyContent="center" rowSpacing={3}>
+                    <Grid item xs={8}>
+                    <Card>
+                        <CardContent>
+                            {blogData.title && 
+                                <Typography gutterBottom variant="h5" component="div">{blogData.title}</Typography>
+                            }
+                            {blogData.body && 
+                                <Typography gutterBottom variant="p" component="div">{blogData.body}</Typography>
+                            }
+                        </CardContent>
+                        <CardActions>
+                            <Link href="/"><Button size="small">Homepage</Button></Link>
+                        </CardActions>
+                    </Card>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Box>
-    </main>
+            </Box>
+        </main>
+    
     </>
   );
+}
+
+export async function getServerSideProps({ params }) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postid}`);
+    const data = await res.json();
+    return {
+      props: {
+        blogData: data,
+      },
+    };
 }
