3836b962ebce05e3158aa4f05b6ea6410b1fef46
Sachin Tiwari <sachintiwariprogrammer@gmail.com>
2023-08-04 13:27:47 +0000

    Updated 1 file via CloudCannon.

--- cloudcannon.config.js
+++ cloudcannon.config.js
@@ -1,15 +1,26 @@
 module.exports = {
+  paths: {
+      collections: 'content',
+      data: 'data',
+      static: 'public',
+      uploads: 'public/uploads'
+  },
   collections_config: {
     posts: {
       name:"Posts",
-      path:"src/pages/posts",
+      path:"content/posts",
       parser:"frontmatter",
       output:true,
       url:"src/pages/posts/[id]",
       _enabled_editors: [
         "data",
         "visual"
-      ]
+      ],
+      schemas: {
+        default: {
+            path: 'schemas/post.md'
+        }
+      }
     },
   },
 };
